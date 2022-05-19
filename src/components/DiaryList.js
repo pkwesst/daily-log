e43d9ccc43f0603e488be3c56dd6e16515fc8e80
pkwesst <pkwesst@gmail.com>
2022-05-19 16:24:41 +0900

    sort

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessdDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessdDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
