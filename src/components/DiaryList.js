044434b4249d2ee1587e96fde642642ec9d7e4bc
pkwesst <pkwesst@gmail.com>
2022-05-20 14:47:35 +0900

    DiaryItem

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
 import MyButton from "./MyButton";
 
 const sortOptionList = [
@@ -84,9 +85,7 @@ const DiaryList = ({ diaryList }) => {
       </div>
 
       {getProcessdDiaryList().map((it) => (
-        <div key={it.id}>
-          {it.content} {it.emotion}
-        </div>
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );
